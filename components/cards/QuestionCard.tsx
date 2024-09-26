c854df9bc496329930e98c883bcb42d8fd388dee
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-09-26 18:02:46 +0530

    Update QuestionCard.tsx

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -73,28 +73,10 @@ const QuestionCard = ({
           alt="user"
           value={
             <span className="flex items-center">
-           {['user_2mWKGv1qqkFoVZiERSgQYsSom2B', 'user_2mZ9MjK0ARsTPzJZUyEC0MWzmf4','user_2mbIauEsU875Pp50WflTE8icx1n'].includes(author.clerkId) ? (
-                <>
+         
                   {author.name}
-                  <svg
-                    aria-label="Verified"
-                    fill="rgb(0, 149, 246)"
-                    height="14"
-                    role="img"
-                    viewBox="0 0 40 40"
-                    width="14"
-                    className="ml-1" // Added margin-left for spacing
-                  >
-                    <title>IT-Verified</title>
-                    <path
-                      d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z"
-                      fillRule="evenodd"
-                    ></path>
-                  </svg>
-                </>
-              ) : (
-                author.name // Just show the name if the condition is not met
-              )}
+              
+            
             </span>
           }
           title={` - asked ${getTimestamp(createdAt)}`}
