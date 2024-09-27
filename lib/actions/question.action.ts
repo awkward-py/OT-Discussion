2c078789cd32a46d396ea44eeaf650f4aae453b2
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-09-27 21:22:02 +0530

    Update question.action.ts

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -42,6 +42,7 @@ export async function getQuestions(params: GetQuestionsParams) {
         query.answers = { $size: 0 }
         break;
       default:
+        sortOptions = { createdAt: - 1 }
         break;
     }
 
