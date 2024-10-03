caf683cc3d3fcbb5cfc3f65c8e6a773c3acbef7e
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-10-03 18:01:45 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,7 +11,7 @@ import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
 import type { Metadata } from 'next';
-import { auth, ClerkProvider } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: 'Home | OT Discussion',
