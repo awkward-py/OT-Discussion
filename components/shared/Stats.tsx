22536a1058cd037aae531dfc8f00542ddfcb2503
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-10-03 22:23:41 +0530

    Update Stats.tsx

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -37,7 +37,7 @@ const Stats = ({ totalQuestions, totalAnswers, badges, reputation }: Props) => {
     WebkitBackgroundClip: 'text',
     WebkitTextFillColor: 'transparent',
   }}
->Snapshot - {reputation}</h4>
+>Snapshot Score - {reputation}</h4>
 
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
