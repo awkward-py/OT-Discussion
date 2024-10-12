99e9f3e4e15126f8ea5e2a98fd4e368d1d044148
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-10-12 15:57:11 +0530

    Update Theme.tsx

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,69 +1,53 @@
-"use client"
+"use client";
 
-import React from 'react'
-
+import React from 'react';
 import { useTheme } from '@/context/ThemeProvider';
-
 import {
   Menubar,
-  MenubarContent,
-  MenubarItem,
   MenubarMenu,
   MenubarTrigger,
-} from "@/components/ui/menubar"
+} from "@/components/ui/menubar";
 import Image from 'next/image';
-import { themes } from '@/constants';
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
 
+
+  const toggleTheme = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light';
+    setMode(newMode);
+    localStorage.theme = newMode; 
+  };
+
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+        <MenubarTrigger
+          className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200"
+          onClick={toggleTheme} 
+        >
           {mode === 'light' ? (
-            <Image 
-            src="/assets/icons/sun.svg" alt="sun" 
-            width={20} 
-            height={20} className="active-theme" 
+            <Image
+              src="/assets/icons/sun.svg"
+              alt="sun"
+              width={20}
+              height={20}
+              className="active-theme"
             />
-          ): (
-          <Image 
-          src="/assets/icons/moon.svg" alt="moon"
-           width={20} 
-           height={20} className="active-theme" 
-           />
-           )}
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              alt="moon"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          )}
         </MenubarTrigger>
-        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border bg-light-900 py-2 dark:border-dark-400 dark:bg-dark-300">
-          {themes.map((item) => (
-            // @ts-ignore
-            <MenubarItem
-              key={item.value}
-              className='flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400'
-              onClick={() => {
-                setMode(item.value);
-                if (item.value !== "system") {
-                  localStorage.theme = item.value;
-                } else {
-                  localStorage.removeItem("theme");
-                }
-              }}
-            >
-              <Image 
-                src={item.icon}
-                alt={item.value}
-                width={16}
-                height={16}
-                className={`${mode === item.value && 'active-theme'}`}
-              />
-              <p className={`body-semibold text-light-500 ${mode === item.value ? 'text-primary-500' : 'text-dark100_light900'}`}>{item.label}</p>
-            </MenubarItem>
-          ))}
-        </MenubarContent>
+      
       </MenubarMenu>
     </Menubar>
-  )
-}
+  );
+};
 
-export default Theme
+export default Theme;
