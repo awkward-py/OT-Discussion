0b1cd589367bc5d82a4464135aedf71793e043d8
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-12-03 09:53:59 +0530

    Update validations.ts

--- lib/validations.ts
+++ lib/validations.ts
@@ -11,9 +11,9 @@ export const AnswerSchema = z.object({
 })
 
 export const ProfileSchema = z.object({
-  name: z.string().min(5).max(50),
-  username: z.string().min(5).max(20),
-  bio: z.string().min(10).max(150),
+  name: z.string().min(5).max(40),
+  username: z.string().min(5).max(15),
+  bio: z.string().min(10).max(100),
   portfolioWebsite: z.string().url(),
   location: z.string().min(2).max(15),
 })
