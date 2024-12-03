963363d94e2800f09fb770391f2f758665943de0
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-12-03 09:58:59 +0530

    Update validations.ts

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,12 +2,12 @@ import * as z from "zod"
 
 export const QuestionsSchema = z.object({
   title: z.string().min(5).max(130),
-  explanation: z.string().min(0),
+  explanation: z.string().min(0).max(130),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 })
 
 export const AnswerSchema = z.object({
-  answer: z.string().min(10)
+  answer: z.string().min(10).max(100)
 })
 
 export const ProfileSchema = z.object({
