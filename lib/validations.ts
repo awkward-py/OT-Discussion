a95747016cd05eeba0a88b460f5eb53339ad90cb
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-12-07 20:12:50 +0530

    Update validations.ts

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,8 +1,8 @@
 import * as z from "zod"
 
 export const QuestionsSchema = z.object({
-  title: z.string().min(5).max(300),
-  explanation: z.string().min(0).max(130),
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(0).max(300),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 })
 
