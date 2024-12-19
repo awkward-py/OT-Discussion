5068659f2a807db8acff07bd887513b406792f64
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-12-19 22:32:42 +0530

    Update page.tsx

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -102,7 +102,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
             {clerkId === userInfo.user.clerkId && (
               <Link href="/profile/edit">
                 <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
-                  Edit Profile
+                  Update Profile
                 </Button>
               </Link>
             )}
